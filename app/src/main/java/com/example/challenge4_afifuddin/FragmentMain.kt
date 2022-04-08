7860a15f58f93eea8e07c9c9dbaab0f80a201a53
afiffdn <fudinafif56@gmail.com>
2022-04-08 16:47:10 +0800

    add function fetch data add and delete

--- app/src/main/java/com/example/challenge4_afifuddin/FragmentMain.kt
+++ app/src/main/java/com/example/challenge4_afifuddin/FragmentMain.kt
@@ -3,8 +3,6 @@ package com.example.challenge4_afifuddin
 import android.app.AlertDialog
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -13,10 +11,11 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
-import com.example.challenge4_afifuddin.database.LoginDatabase
 import com.example.challenge4_afifuddin.database.recipe.Resep
+import com.example.challenge4_afifuddin.database.recipe.ResepAdapter
 import com.example.challenge4_afifuddin.database.recipe.ResepDatabase
 import com.example.challenge4_afifuddin.databinding.AddDataBinding
+import com.example.challenge4_afifuddin.databinding.EditDataBinding
 import com.example.challenge4_afifuddin.databinding.FragmentMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.InternalCoroutinesApi
@@ -37,6 +36,10 @@ class FragmentMain : Fragment() {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
+    }
+    override fun onResume() {
+        super.onResume()
+        fetchData()
     }
 
     @OptIn(InternalCoroutinesApi::class)
@@ -96,15 +99,109 @@ class FragmentMain : Fragment() {
 
     }
 
+    @OptIn(InternalCoroutinesApi::class)
     private fun fetchData() {
         lifecycleScope.launch(Dispatchers.IO){
             val listResep = mDb?.resepdao()?.getAllRecipe()
             activity?.runOnUiThread(){
             listResep?.let {
+                val adapter = ResepAdapter(
+                    it,
+                    onEditClickListener = {resep ->
+                        val editdialog = EditDataBinding.inflate(LayoutInflater.from(requireContext()))
+                        val dialogBuilder = AlertDialog.Builder(requireContext())
+                        dialogBuilder.setView(editdialog.root)
+                        val dialog = dialogBuilder.create()
+                        dialog.setCancelable(false)
+                        editdialog.etInsertFoodName.setText(resep.name)
+                        editdialog.etInsertIngredient.setText(resep.ingredient)
+                        editdialog.etInsertStep.setText(resep.step)
+                        editdialog.btnCancel.setOnClickListener {
+                            dialog.dismiss()
+                        }
+
+                        editdialog.btnEdit.setOnClickListener {
+                        val myrecipe = ResepDatabase.getInstance(requireContext())
+                            val resep = Resep(
+                                null,
+                                editdialog.etInsertFoodName.text.toString(),
+                                editdialog.etInsertIngredient.text.toString(),
+                                editdialog.etInsertStep.text.toString()
+                            )
+                            lifecycleScope.launch(Dispatchers.IO){
+                            val result = myrecipe?.resepdao()?.updateRecipe(resep)
+                                runBlocking  (Dispatchers.Main){
+                                    if (result != 0){
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "resep ${resep.name} berhasil di update",
+                                            Toast.LENGTH_SHORT
+                                        )
+                                            .show()
+                                            fetchData()
+                                    }else{
+                                        Toast.makeText(
+                                            requireContext(),
+                                            "resep ${resep.name} gagal di update",
+                                            Toast.LENGTH_SHORT
+                                        )
+                                            .show()
+                                        dialog.dismiss()
+                                    }
+                                }
+                            }
+
+                        }
+
+                        dialog.show()
+                    },
+                    onDeleteClickListener ={resep ->
+                        AlertDialog.Builder(requireContext())
+                            .setPositiveButton("Iya"){_,_->
+                        val mDb = ResepDatabase.getInstance(requireContext())
+                                lifecycleScope.launch(Dispatchers.IO){
+                                val result = mDb?.resepdao()?.deleteRecipe(resep)
+                                    activity?.runOnUiThread{
+                                        if(result != 0){
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "${resep.name} berhasil dihapus",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            Toast.makeText(
+                                                requireContext(),
+                                                "${resep.name} gagal dihapus",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    }
+                                    fetchData()
+                                }
+                            }
+                            .setNegativeButton("Tidak"){dialog, _ ->dialog.dismiss()
+                            }
+                            .setMessage("Apakah anda akan menghapus resep ${resep.name}")
+                            .setTitle("konfirmasi hapus")
+                            .create()
+                            .show()
+                    }
+
+
+                )
+                binding.recycleview.adapter = adapter
 
             }
             }
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        ResepDatabase.destroyInstance()
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
